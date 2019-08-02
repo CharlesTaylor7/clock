635462dca6d1a830c1b8447eeb063a2fd2aa958c
Charles Taylor <charlest@paragontruss.com>
2019-08-01 21:28:55 -0400

    Sets time on refresh

--- src/App.js
+++ src/App.js
@@ -14,38 +14,52 @@ const HourMark = (hour) => (
   />
 );
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <svg
-        className="clock"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 100 100"
-      >
-        <circle cx="50" cy="50" r="45" stroke="black" strokeWidth="2" fill="white" />
-        <circle cx="50" cy="50" r="2" stroke="black" />
-        <line
-          className="hour-hand"
-          x1="50"
-          x2="50"
-          y1="20"
-          y2="50"
-          stroke="black"
-          strokeWidth="2"
-        />
-        <line
-          className="minute-hand"
-          x1="50"
-          x2="50"
-          y1="20"
-          y2="42.5"
-          stroke="black"
-          strokeWidth="2"
-        />
-        {[...Array(12).keys()].map(HourMark)}
-      </svg>
-    </header>
-  </div>
-);
+const App = () => {
+  const now = new Date();
+  const hours = now.getHours() % 12;
+  const minutes = now.getMinutes();
+  // const seconds = now.getSeconds();
+
+  const minuteHandPercentage = minutes / 60;
+  const hourHandPercentage = hours / 12;
+
+  const rotate = percentage => `rotate(${360 * percentage}, 50, 50)`;
+ debugger;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <svg
+          className="clock"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+        >
+          <circle cx="50" cy="50" r="45" stroke="black" strokeWidth="2" fill="white" />
+          <circle cx="50" cy="50" r="2" stroke="black" />
+          <line
+            className="hour-hand"
+            transform={rotate(hourHandPercentage)}
+            x1="50"
+            x2="50"
+            y1="20"
+            y2="50"
+            stroke="black"
+            strokeWidth="2"
+          />
+          <line
+            className="minute-hand"
+            transform={rotate(minuteHandPercentage)}
+            x1="50"
+            x2="50"
+            y1="27.5"
+            y2="50"
+            stroke="black"
+            strokeWidth="2"
+          />
+          {[...Array(12).keys()].map(HourMark)}
+        </svg>
+      </header>
+    </div>
+  );
+};
 
 export default App;
