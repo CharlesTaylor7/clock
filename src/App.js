fce97b82370964c3767cf2ccbd412d6dc224b3ac
Charles Taylor <charlest@paragontruss.com>
2019-08-01 21:12:27 -0400

    Basic clock built with css & svg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const HourMark = (hour) => (
+  <line
+    key={hour}
+    transform={`rotate(${hour * 360 / 12}, 50, 50)`}
+    x1="50"
+    x2="50"
+    y1="8"
+    y2="15"
+    stroke="black"
+    strokeWidth="2"
+  />
+);
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <svg
+        className="clock"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+      >
+        <circle cx="50" cy="50" r="45" stroke="black" strokeWidth="2" fill="white" />
+        <circle cx="50" cy="50" r="2" stroke="black" />
+        <line
+          className="hour-hand"
+          x1="50"
+          x2="50"
+          y1="20"
+          y2="50"
+          stroke="black"
+          strokeWidth="2"
+        />
+        <line
+          className="minute-hand"
+          x1="50"
+          x2="50"
+          y1="20"
+          y2="42.5"
+          stroke="black"
+          strokeWidth="2"
+        />
+        {[...Array(12).keys()].map(HourMark)}
+      </svg>
+    </header>
+  </div>
+);
 
 export default App;
