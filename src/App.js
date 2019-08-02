688640f3f7485257abc90fc1510c1dd7ec9131f2
Charles Taylor <charlest@paragontruss.com>
2019-08-01 22:09:44 -0400

    Add ticking sound

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Rx from 'rxjs/operators';
+import * as Observable from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
 import './App.css';
+import tick from './clock-tick.mp3';
+import Sound from 'react-sound';
+
+const TickSound = ({ on }) => (
+  <Sound
+    url={tick}
+    playStatus={"PLAYING"}
+  />
+);
 
 const HourMark = (hour) => (
   <line
@@ -14,8 +26,15 @@ const HourMark = (hour) => (
   />
 );
 
+const date = new Date();
 const App = () => {
-  const now = new Date();
+  const [now, setNow] = useState(date);
+  useEffect(() => {
+    setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+  }, []);
+
   const hours = now.getHours() % 12;
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
@@ -24,10 +43,10 @@ const App = () => {
   const hourHandPercentage = (hours + minuteHandPercentage) / 12;
 
   const rotate = percentage => `rotate(${360 * percentage}, 50, 50)`;
- debugger;
   return (
     <div className="App">
       <header className="App-header">
+        <TickSound on={now} />
         <svg
           className="clock"
           xmlns="http://www.w3.org/2000/svg"
