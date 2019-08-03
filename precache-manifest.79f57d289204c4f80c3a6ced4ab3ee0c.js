0639c9b1c4e9c90383d943401819f6847e437feb
Charles Taylor <charlest@paragontruss.com>
2019-08-03 12:17:04 -0400

    Updates

---
+++ precache-manifest.79f57d289204c4f80c3a6ced4ab3ee0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a585863139f6eb8e71342f205fcead",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "6fcdd13f12e3642ac91c",
+    "url": "/clock/static/css/main.7a76e941.chunk.css"
+  },
+  {
+    "revision": "f20780fd46bfdb8ecb26",
+    "url": "/clock/static/js/2.9ca9f71b.chunk.js"
+  },
+  {
+    "revision": "6fcdd13f12e3642ac91c",
+    "url": "/clock/static/js/main.6d3f5a49.chunk.js"
+  },
+  {
+    "revision": "1d811e2acbdbe64bfd52",
+    "url": "/clock/static/js/runtime~main.4586ffc4.js"
+  },
+  {
+    "revision": "70ac3814dd819491e3ea514481a1807e",
+    "url": "/clock/static/media/clock-tick.70ac3814.mp3"
+  }
+]);
