07eea1e2fba4752f345668a4168eb6012b751a1f
Charles Taylor <charlestaylor95@gmail.com>
2019-09-21 01:21:27 -0400

    Use api that supports https

--- src/api/time.js
+++ src/api/time.js
@@ -1,4 +1,12 @@
 export default () =>
-  fetch('https://worldclockapi.com/api/json/est/now')
-    .then(res => res.json())
-    .then(json => new Date(json.currentDateTime));
+  fetch(
+    'https://world-clock.p.rapidapi.com/json/utc/now',
+    {
+      headers: {
+        "x-rapidapi-host": "world-clock.p.rapidapi.com",
+        "x-rapidapi-key": "e9d0ac9d9bmsh10f31b0dfd5c1efp1691a7jsnccfcbe76871e",
+      }
+    }
+  )
+  .then(res => res.json())
+  .then(json => new Date(json.currentDateTime));
