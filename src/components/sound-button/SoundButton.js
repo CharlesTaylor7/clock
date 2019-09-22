7e047dbe2a41a419af46b828fbff798cd7923eb1
Charles Taylor <charlestaylor95@gmail.com>
2019-09-21 22:39:35 -0400

    Simplify soundbutton

--- src/components/sound-button/SoundButton.js
+++ src/components/sound-button/SoundButton.js
@@ -4,22 +4,14 @@ import soundEnabledIcon from './sound-enabled.svg'
 import soundDisabledIcon from './sound-disabled.svg'
 import { connect } from 'react-redux'
 
-const _SoundButton = ({ soundOn, dispatch }) => {
-
-  const toggleSound = () =>
-    dispatch({ type: 'SET_SOUND', on: !soundOn });
-
-  const icon = soundOn ? soundEnabledIcon : soundDisabledIcon;
-
-  return (
-    <img
-      className="sound-button"
-      src={icon}
-      onClick={toggleSound}
-      alt={"Toggle sound"}
-    />
-  )
-}
+const _SoundButton = ({ soundOn, dispatch }) => (
+  <img
+    className="sound-button"
+    alt={"Toggle sound"}
+    src={soundOn ? soundEnabledIcon : soundDisabledIcon}
+    onClick={() => dispatch({ type: 'SET_SOUND', on: !soundOn })}
+  />
+)
 
 const mapStateToProps = (state) => ({
   soundOn: state.soundOn
