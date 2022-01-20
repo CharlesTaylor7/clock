0c47b8aaf1362b9877ba0ef63610ea075b4a18a5
Charles Taylor <charlestaylor95@gmail.com>
2022-01-20 14:18:28 -0500

    Updates

---
+++ precache-manifest.f9f9d1ea6500a2c7d59ef8b7630a4772.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7d409250d9c7b7fd786870517d7a80",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "08078b18a07951e6f158",
+    "url": "/clock/static/css/main.1826ffa2.chunk.css"
+  },
+  {
+    "revision": "a09c77e1fc2f12f8609f",
+    "url": "/clock/static/js/2.62d843bb.chunk.js"
+  },
+  {
+    "revision": "08078b18a07951e6f158",
+    "url": "/clock/static/js/main.2cac798a.chunk.js"
+  },
+  {
+    "revision": "1d811e2acbdbe64bfd52",
+    "url": "/clock/static/js/runtime~main.4586ffc4.js"
+  },
+  {
+    "revision": "70ac3814dd819491e3ea514481a1807e",
+    "url": "/clock/static/media/clock-tick.70ac3814.mp3"
+  },
+  {
+    "revision": "40992a0f88ec7ebe6d2d2b30c9118934",
+    "url": "/clock/static/media/sound-disabled.40992a0f.svg"
+  },
+  {
+    "revision": "a53e18456469c89863f9fbe75f6a74ad",
+    "url": "/clock/static/media/sound-enabled.a53e1845.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clock/precache-manifest.43afea3c504842f741034cd3fc86bbef.js"
+  "/clock/precache-manifest.f9f9d1ea6500a2c7d59ef8b7630a4772.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clock/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
